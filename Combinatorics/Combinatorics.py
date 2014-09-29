7c06d387237006da8b525a1954ab6f171f071516
andres-liiver <andres.liiver@gmail.com>
2014-09-29 20:13:48 +0300

    Updated combinatorics.py code.

--- Combinatorics/Combinatorics.py
+++ Combinatorics/Combinatorics.py
@@ -1,52 +1,75 @@
-from math import factorial
-
-def variations(items, r):
+def variations(items, r, strResult = False):
     '''
     Calculates all r length variations of items 
     @arg items - list of items
     @arg r - length of variations
-    @returns two-dimensional list of variations
+    @arg strResult - if True, returns result as multi-line string
+    @returns two-dimensional list of variations or multi-line string
     '''
+    if items == []:
+        return []
+
     if r > len(items):
         return "variations: r cannot be bigger than the number of items!"
 
     if r == 1:
         return [[item] for item in items]
 
-    result = []
-    temp1 = []; temp2 = []
-    numEach = factorial(len(items)) // factorial(len(items) - r) // len(items)
+    result = []; subVariations = []
 
     for i, item in enumerate(items):
-        temp1 = [[item] for _ in range(numEach)]
-        temp2 = variations(items[:i] + items[i+1 :], r-1)
-        result += [temp1[i] + temp2[i] for i in range(len(temp1))]
+        subVariations = variations(items[:i] + items[i+1 :], r-1)
+        if subVariations == []: break
+        for item2 in subVariations:
+            result += [[item] + item2]
 
-    return result
+    if strResult:
+        resultStr = ""
+        for subList in result:
+            resultStr += ', '.join(str(item) for item in subList)
+            resultStr += '\n'
 
-def combinations(items, r):
+        return resultStr
+    else:
+        return result
+
+def combinations(items, r, strResult = False):
     '''
     Calculates all r length combinations of items 
     @arg items - list of items
     @arg r - length of combinations
-    @returns two-dimensional list of combinations
+    @arg strResult - if True, returns result as multi-line string
+    @returns two-dimensional list of combinations or multi-line string
     '''
+    if items == []:
+        return []
+
     if r > len(items):
-        return "variations: r cannot be bigger than the number of items!"
+        if strResult:
+            return "combinations: r cannot be bigger than the number of items!"
+        else:
+            return []
 
     if r == 1:
         return [[item] for item in items]
 
-    result = []
-    temp1 = []; temp2 = []
-    numEach = factorial(len(items)) // factorial(len(items) - r) // len(items)
+    result = []; subCombinations = []
 
     for i, item in enumerate(items):
-        temp1 = [[item] for _ in range(numEach)]
-        temp2 = variations(items[:i] + items[i+1 :], r-1)
-        result += [temp1[i] + temp2[i] for i in range(len(temp1))]
+        subCombinations = combinations(items[i+1:], r-1)
+        if subCombinations == []: break
+        for item2 in subCombinations:
+            result += [[item] + item2]
 
-    return result
+    if strResult:
+        resultStr = ""
+        for subList in result:
+            resultStr += ', '.join(str(item) for item in subList)
+            resultStr += '\n'
 
-result = variations([1,2,3,4,5], 2)
-for subList in result: print(', '.join(str(item) for item in subList))
+        return resultStr
+    else:
+        return result
+
+print(variations([1,2,3,4,5], 3, True))
+print(combinations([1,2,3,4,5], 3, True))
